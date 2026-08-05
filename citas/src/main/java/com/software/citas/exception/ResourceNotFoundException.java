a33f81eec2b721202f4cee1961674f6424d2f66f
elian enrique ureche lopez <elian.official2000@gmail.com>
2026-08-05 13:02:33 +0200

    saque el proyecto de la carpeta citas a la raiz

--- citas/src/main/java/com/software/citas/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.software.citas.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-}
