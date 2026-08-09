2cf0cdd24c3b37cc0abba72a04968ba0458c4097
elian enrique ureche lopez <elian.official2000@gmail.com>
2026-08-09 21:38:23 +0200

    se agrega los permisos para que el front pueda consumir nuestra api

--- src/main/java/com/software/citas/config/ConfigCors.java
+++ src/main/java/com/software/citas/config/ConfigCors.java
@@ -23,7 +23,8 @@ public class ConfigCors {
                 "http://localhost:5173",   // Vite
                 "http://localhost:4200",   // Angular
                 "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "http://localhost:5500",
+                "https://frontendcitas-production.up.railway.app"
         ));
 
         config.setAllowedHeaders(List.of("*"));
